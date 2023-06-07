e720e872b57c5f9ee0f3fab5d2ab4bf6838b7446
Mert Akca <mert.akca@plentific.com>
2023-06-07 23:50:48 +0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { Main } from '../components/layouts/main'
 import { theme } from '../lib/theme'
 import Fonts from '../lib/fonts'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
     return (
-        <ChakraProvider theme={theme}>
-            <Fonts />
-            <Main router={router}>
-                <Component {...pageProps} />
-            </Main>
-        </ChakraProvider>
+        <>
+            <ChakraProvider theme={theme}>
+                <Fonts />
+                <Main router={router}>
+                    <Component {...pageProps} />
+                </Main>
+            </ChakraProvider>
+            <Analytics />
+        </>
     )
 }
 
